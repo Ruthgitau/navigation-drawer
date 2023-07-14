0acb094623b96d9c03ef2cbf2c716b75119921f7
Ruthgitau <gitaur72@gmail.com>
2023-07-14 10:56:55 +0300

    navigation menu

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.navigationdrawer
+
 
 import android.annotation.SuppressLint
 import android.app.Notification.Action
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -16,24 +17,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.navigationdrawer.R
 import com.example.navigationdrawer.R.layout.activity_main
 import com.example.navigationdrawer.ui.theme.NavigationDrawerTheme
+class MainActivity : ComponentActivity() {
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
-class MainActivity : AppCompatActivity() {
 
+    private fun ActionBarDrawerToggle(mainActivity: MainActivity): ActionBarDrawerToggle {
+        TODO("Not yet implemented")
+    }
 
-
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main)
-        val navController=findNavController(R.id.nav_host_fragment_content_main)
-        navController.navigate(R.id.action_homeFragment_to_blankFragment)
+
+        drawerLayout=findViewById(drawerLayout)
+        actionBarDrawerToggle=ActionBarDrawerToggle(this) drawerLayout  R.string.nav_open
 
     }
 
-}
+
+
+
+    }
+
+
+
+
 
 
